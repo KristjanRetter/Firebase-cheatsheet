a95606d62df2602a2215303b4e5b35f78d2dc1cc
kristjan <kristjan.retter@outlook.com>
2024-09-07 14:32:08 +0300

    ch

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,6 +7,41 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase:
     "https://github.com/KristjanRetter/Firebase-cheatsheet/blob/main/",
+
+  navbar: {
+    extraContent: (
+      <>
+        <a
+          style={{
+            fontSize: "12px",
+          }}
+          href="https://x.com/KristjanRetter"
+          target="_blank"
+        >
+          <span
+            style={{
+              fontSize: "12px",
+            }}
+          >
+            Made with ☕ by{" "}
+          </span>{" "}
+          <span
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              padding: "2px 10px",
+              borderRadius: "20px",
+
+              fontSize: "12px",
+            }}
+          >
+            Kristjan
+            <span style={{ fontSize: "16px", marginLeft: "4px" }}>𝕏</span>
+          </span>
+        </a>
+      </>
+    ),
+  },
 };
 
 export default config;
